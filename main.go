cc74281505340e71f87671304fb67b37c46fb468
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2026-04-10 11:33:04 -0400

    Update main.go

--- main.go
+++ main.go
@@ -11,9 +11,8 @@ import (
 func main() {
 
 	// Define a globally unique designation that will be used to identify this bridge server.
-// Our peer's ID will become "bridge@[DESIGNATION]".
-	const DESIGNATION = "SOMEWHERE-EARTH"
 	// Our peer's ID will become "bridge@[DESIGNATION]".
+	const DESIGNATION = "SOMEWHERE-EARTH"
 
 	instance := server.New(DESIGNATION, &server.Config{
 		Enable_MOTD:        true,
